a922f1aa09ee1b639fb61ca8e911f69d5200e5dd
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-07-09 21:51:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import styles from "./App.module.css";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className={styles.App}>
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
